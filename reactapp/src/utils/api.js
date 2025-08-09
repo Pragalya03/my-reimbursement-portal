b3b23ba7a7e0fea4c006e3965bc703ac6b920121
Pragalya03 <pragalya03@gmail.com>
2025-08-09 22:24:29 +0530

    med 1

--- reactapp/src/utils/api.js
+++ reactapp/src/utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080/api/expenses";
+const BASE_URL = "https://8080-faedbbbbecaaddcbcedcecbaebefef.premiumproject.examly.io/api/expenses";
 
 export async function createExpense(expense) {
   const res = await fetch(BASE_URL, {
