820a4925298f6719a0b016f9ffb56a7f4168c359
Pragalya03 <149648960+Pragalya03@users.noreply.github.com>
2025-08-11 00:01:59 +0530

    adding user type selection to App.js

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -4,14 +4,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ExpenseForm from "./components/ExpenseForm";
 import ExpenseList from "./components/ExpenseList";
 import ExpenseStatusUpdate from "./components/ExpenseStatusUpdate";
+import UserTypeSelection from "./components/UserTypeSelection";
 
 function App() {
   return (
     <Router>
-
       <Routes>
-        <Route path="/" element={<ExpenseForm />} />
-        <Route path="/expenses" element={<ExpenseList />} />
+        <Route path="/" element={<UserTypeSelection />} />
+        <Route path="/employee" element={<ExpenseForm />} />
+        <Route path="/manager" element={<ExpenseList />} />
         <Route path="/expenses/:id/update" element={<ExpenseStatusUpdate />} />
       </Routes>
     </Router>
