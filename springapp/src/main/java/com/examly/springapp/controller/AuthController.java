b619faffdcaff77e7658807df5a2ce422527ce80
Pragalya03 <149648960+Pragalya03@users.noreply.github.com>
2025-08-15 18:28:22 +0530

    Update AuthController.java

--- springapp/src/main/java/com/examly/springapp/controller/AuthController.java
+++ springapp/src/main/java/com/examly/springapp/controller/AuthController.java
@@ -4,50 +4,46 @@ import com.examly.springapp.model.User;
 import com.examly.springapp.service.UserService;
 import com.examly.springapp.security.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.*;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
-    private final UserService userService;
-    private final JwtUtil jwtUtil;
+    @Autowired
+    private UserService userService;
 
     @Autowired
-    public AuthController(UserService userService, JwtUtil jwtUtil) {
-        this.userService = userService;
-        this.jwtUtil = jwtUtil;
-    }
+    private AuthenticationManager authManager;
 
-    /**
-     * Register a new user
-     */
+    @Autowired
+    private JwtUtil jwtUtil;
+
     @PostMapping("/register")
     public User register(@RequestBody User user) {
-        // This will hash the password and save the user
         return userService.registerUser(user);
     }
 
-    /**
-     * Login user and return JWT token
-     */
     @PostMapping("/login")
     public Map<String, String> login(@RequestBody LoginRequest request) {
-        User user = userService.loginUser(request.getUsernameOrEmail(), request.getPassword());
+        authManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getUsernameOrEmail(), request.getPassword())
+        );
+
+        User user = userService.getUserByUsername(request.getUsernameOrEmail());
+
         String token = jwtUtil.generateToken(user.getUsername());
-        return Map.of("token", token); // {"token": "jwt_here"}
+        return Map.of("token", token);
     }
 
-    // DTO for login request
     public static class LoginRequest {
         private String usernameOrEmail;
         private String password;
-
         public String getUsernameOrEmail() { return usernameOrEmail; }
         public void setUsernameOrEmail(String usernameOrEmail) { this.usernameOrEmail = usernameOrEmail; }
-
         public String getPassword() { return password; }
         public void setPassword(String password) { this.password = password; }
     }
