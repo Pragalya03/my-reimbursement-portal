2ef561c4a4105a276db581d0a25b1c1b8f49bca5
Pragalya03 <149648960+Pragalya03@users.noreply.github.com>
2025-08-15 12:10:18 +0530

    Update ReceiptService.java

--- springapp/src/main/java/com/examly/springapp/service/ReceiptService.java
+++ springapp/src/main/java/com/examly/springapp/service/ReceiptService.java
@@ -3,7 +3,6 @@ package com.examly.springapp.service;
 import com.examly.springapp.model.Receipt;
 import com.examly.springapp.repository.ReceiptRepository;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
