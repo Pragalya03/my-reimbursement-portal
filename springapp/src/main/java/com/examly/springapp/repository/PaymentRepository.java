3b1e159eff111f425c63ba94b0b38fac524d5759
Pragalya03 <149648960+Pragalya03@users.noreply.github.com>
2025-08-15 11:35:46 +0530

    Create PaymentRepository.java

---
+++ springapp/src/main/java/com/examly/springapp/repository/PaymentRepository.java
@@ -0,0 +1,17 @@
+package com.examly.springapp.repository;
+
+import com.examly.springapp.model.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    List<Payment> findByExpenseId(Long expenseId);
+
+    List<Payment> findByStatus(String status);
+
+    List<Payment> findByPaymentMethod(String paymentMethod);
+
+    List<Payment> findByProcessedBy(Long processedBy);
+}
