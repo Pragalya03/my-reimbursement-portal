a3cb4aacd3af6cd72894eb9dd95922aa2225a885
Pragalya03 <149648960+Pragalya03@users.noreply.github.com>
2025-08-15 14:19:04 +0530

    Update ReceiptManagement.jsx

---
+++ reactapp/src/pages/ReceiptManagement.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReceiptUpload from "../components/ReceiptUpload";
+
+function ReceiptManagement() {
+  return (
+    <div className="receipt-management">
+      <h2>Receipt Management</h2>
+      <ReceiptUpload />
+    </div>
+  );
+}
+
+export default ReceiptManagement;
