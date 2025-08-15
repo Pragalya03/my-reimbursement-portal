84a0a665eb0e68633306ac9cc1a6c98c17865315
Pragalya03 <149648960+Pragalya03@users.noreply.github.com>
2025-08-15 11:34:43 +0530

    Create ExpenseCategoryRepository.java

---
+++ springapp/src/main/java/com/examly/springapp/repository/ExpenseCategoryRepository.java
@@ -0,0 +1,16 @@
+package com.examly.springapp.repository;
+
+import com.examly.springapp.model.ExpenseCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ExpenseCategoryRepository extends JpaRepository<ExpenseCategory, Long> {
+
+    Optional<ExpenseCategory> findByCategoryCode(String categoryCode);
+
+    List<ExpenseCategory> findByIsActiveTrue();
+
+    List<ExpenseCategory> findByParentCategoryId(Long parentCategoryId);
+}
