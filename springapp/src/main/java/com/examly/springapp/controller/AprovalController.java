66c38cf738cb5ab88c75b600e9e88b0302bbee39
Pragalya03 <149648960+Pragalya03@users.noreply.github.com>
2025-08-15 11:51:54 +0530

    Update AprovalController.java

--- springapp/src/main/java/com/examly/springapp/controller/AprovalController.java
+++ springapp/src/main/java/com/examly/springapp/controller/AprovalController.java
@@ -1,56 +1,41 @@
 package com.examly.springapp.controller;
 
 import com.examly.springapp.model.Approval;
-import com.examly.springapp.repository.ApprovalRepository;
+import com.examly.springapp.service.ApprovalService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/approvals")
 public class ApprovalController {
 
     @Autowired
-    private ApprovalRepository approvalRepository;
+    private ApprovalService approvalService;
 
     @GetMapping
     public List<Approval> getAllApprovals() {
-        return approvalRepository.findAll();
+        return approvalService.getAllApprovals();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Approval> getApprovalById(@PathVariable Long id) {
-        Optional<Approval> approval = approvalRepository.findById(id);
-        return approval.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    public Approval getApprovalById(@PathVariable Long id) {
+        return approvalService.getApprovalById(id);
     }
 
     @PostMapping
     public Approval createApproval(@RequestBody Approval approval) {
-        return approvalRepository.save(approval);
+        return approvalService.createApproval(approval);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Approval> updateApproval(@PathVariable Long id, @RequestBody Approval approvalDetails) {
-        return approvalRepository.findById(id).map(approval -> {
-            approval.setExpenseId(approvalDetails.getExpenseId());
-            approval.setApproverId(approvalDetails.getApproverId());
-            approval.setApprovalLevel(approvalDetails.getApprovalLevel());
-            approval.setApprovalStatus(approvalDetails.getApprovalStatus());
-            approval.setApprovalDate(approvalDetails.getApprovalDate());
-            approval.setComments(approvalDetails.getComments());
-            approval.setIsFinalApproval(approvalDetails.getIsFinalApproval());
-            return ResponseEntity.ok(approvalRepository.save(approval));
-        }).orElseGet(() -> ResponseEntity.notFound().build());
+    public Approval updateApproval(@PathVariable Long id, @RequestBody Approval approval) {
+        return approvalService.updateApproval(id, approval);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteApproval(@PathVariable Long id) {
-        return approvalRepository.findById(id).map(approval -> {
-            approvalRepository.delete(approval);
-            return ResponseEntity.ok().build();
-        }).orElseGet(() -> ResponseEntity.notFound().build());
+    public void deleteApproval(@PathVariable Long id) {
+        approvalService.deleteApproval(id);
     }
 }
