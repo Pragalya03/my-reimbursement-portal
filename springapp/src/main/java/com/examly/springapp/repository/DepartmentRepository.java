6e91aa918d9d6d4562c5dd16e166c4a424b46964
Pragalya03 <149648960+Pragalya03@users.noreply.github.com>
2025-08-15 11:34:18 +0530

    Create DepartmentRepository.java

---
+++ springapp/src/main/java/com/examly/springapp/repository/DepartmentRepository.java
@@ -0,0 +1,16 @@
+package com.examly.springapp.repository;
+
+import com.examly.springapp.model.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    Optional<Department> findByDepartmentCode(String departmentCode);
+
+    List<Department> findByIsActiveTrue();
+
+    List<Department> findByManagerId(Long managerId);
+}
