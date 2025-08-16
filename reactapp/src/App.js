29bc4ff6d4a7353997c38b7bc4ad07a24212ce07
Pragalya03 <pragalya03@gmail.com>
2025-08-16 13:09:28 +0000

    bringing back old app.js

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,21 +1,95 @@
-// App.js
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+/*import React from "react";
+import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
 
-// Import your pages
-import Register from "./pages/Register";
-import AdminPanel from "./pages/AdminPanel";
 import ExpenseForm from "./components/ExpenseForm";
+import ExpenseList from "./components/ExpenseList";
+import ExpenseStatusUpdate from "./components/ExpenseStatusUpdate";
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/admin" element={<AdminPanel />} />
-        <Route path="/expenses/new" element={<ExpenseForm />} />
-        {/* You can add more routes here if needed */}
-      </Routes>
+      <nav className="full-width-navbar">
+        <NavLink to="/employee" className="user-type-btn">
+          Employee
+        </NavLink>
+        <NavLink to="/manager" className="user-type-btn">
+          Manager
+        </NavLink>
+      </nav>
+
+      <div className="App">
+        <Routes>
+          <Route
+             path="/"
+                element={
+                    <div className="page-container">
+                        <ExpenseForm />
+                    </div>
+              }
+          />
+          <Route
+             path="/employee"
+                element={
+                    <div className="page-container">
+                        <ExpenseForm />
+                    </div>
+              }
+          />
+
+          <Route path="/manager" element={<ExpenseList />} />
+          <Route path="/expenses/:id/update" element={<ExpenseStatusUpdate />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+
+export default App;
+*/import React from "react";
+import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
+import { appRoutes } from "./routes";
+import './App.css';
+import Register from "./pages/Register";
+import AdminPanel from "./pages/AdminPanel";
+import ExpenseForm from "./components/ExpenseForm";
+function App() {
+  const currentUser = getCurrentUser();
+
+  return (
+    <Router>
+      <nav className="full-width-navbar">
+        <NavLink to="/employee" className="user-type-btn">Employee</NavLink>
+        <NavLink to="/manager" className="user-type-btn">Manager</NavLink>
+        <NavLink to="/finance" className="user-type-btn">Finance</NavLink>
+        <NavLink to="/admin" className="user-type-btn">Admin</NavLink>
+      </nav>
+
+      <div className="App">
+        <Routes>
+          {appRoutes.map((route, idx) => {
+            const Element = route.element;
+            if (route.roles) {
+              return (
+                <Route
+                  key={idx}
+                  path={route.path}
+                  element={
+                    <ProtectedRoute
+                      element={Element}
+                      allowedRoles={route.roles}
+                      currentUser={currentUser}
+                    />
+                  }
+                />
+              );
+            } else {
+              return <Route key={idx} path={route.path} element={<Element />} />;
+            }
+          })}
+        </Routes>
+      </div>
     </Router>
   );
 }
