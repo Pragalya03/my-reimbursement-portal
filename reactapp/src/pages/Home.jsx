18913a6043186fce1570531491f536fae2f7b63d
Pragalya03 <149648960+Pragalya03@users.noreply.github.com>
2025-08-19 13:49:56 +0530

    Update Home.jsx

--- reactapp/src/pages/Home.jsx
+++ reactapp/src/pages/Home.jsx
@@ -1,3 +1,31 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import "../styles/Home.css";
+
+// function Home() {
+//   return (
+//     <div className="home-page">
+//       <h1>Welcome to Expense Management System</h1>
+//       <p>Track, submit, and approve expenses with ease.</p>
+//       <div className="home-buttons">
+//         <Link to="/login">
+//           <button>Login</button>
+//         </Link>
+//         <Link to="/register">
+//           <button>Register</button>
+//         </Link>
+//         {/* <div className="nav-right">
+//         <Link to="/admin">
+//           <button>Admin</button>
+//           </Link>
+//         </div> */}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Home;
+
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Home.css";
@@ -14,14 +42,10 @@ function Home() {
         <Link to="/register">
           <button>Register</button>
         </Link>
-        {/* <div className="nav-right">
-        <Link to="/admin">
-          <button>Admin</button>
-          </Link>
-        </div> */}
       </div>
     </div>
   );
 }
 
 export default Home;
+
