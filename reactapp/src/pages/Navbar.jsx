4a24b1ec0a18f22d96c733406069c5042d9bdb11
Pragalya03 <149648960+Pragalya03@users.noreply.github.com>
2025-08-19 18:00:07 +0530

    Create Navbar.jsx

---
+++ reactapp/src/pages/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="brand">💼 ExpenseSys</div>
+      <div className="nav-links">
+        <h2>ClaimEase</h2>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
