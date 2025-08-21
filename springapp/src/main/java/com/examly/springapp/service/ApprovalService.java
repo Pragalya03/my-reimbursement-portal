48513917c073c6365448bbdf2918e1ff26b9eef2
Pragalya03 <149648960+Pragalya03@users.noreply.github.com>
2025-08-21 21:13:19 +0530

    Update ApprovalService.java

--- springapp/src/main/java/com/examly/springapp/service/ApprovalService.java
+++ springapp/src/main/java/com/examly/springapp/service/ApprovalService.java
@@ -8,34 +8,26 @@ import java.util.List;
 
 @Service
 public class ApprovalService {
-
     private final ApprovalRepository approvalRepository;
-
     public ApprovalService(ApprovalRepository approvalRepository) {
         this.approvalRepository = approvalRepository;
     }
-
     public List<Approval> getAllApprovals() {
         return approvalRepository.findAll();
     }
-
     public Approval getApprovalById(Long id) {
         return approvalRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Approval not found with id " + id));
     }
-
     public Approval createApproval(Approval approval) {
         return approvalRepository.save(approval);
     }
-
     public Approval updateApproval(Long id, Approval approvalDetails) {
         Approval approval = getApprovalById(id);
         approval.setApprovalStatus(approvalDetails.getApprovalStatus());
         approval.setComments(approvalDetails.getComments());
         approval.setApprovalDate(approvalDetails.getApprovalDate());
         approval.setIsFinalApproval(approvalDetails.getIsFinalApproval());
-        // approval.setApprovalLevel(approvalDetails.getApprovalLevel());
-        // approval.setApprover(approvalDetails.getApprover());
         approval.setExpense(approvalDetails.getExpense());
         return approvalRepository.save(approval);
     }
