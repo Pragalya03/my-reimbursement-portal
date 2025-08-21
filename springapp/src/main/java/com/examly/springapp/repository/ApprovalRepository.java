77f11f99020920122c3c08365531876e3ca881e6
Pragalya03 <149648960+Pragalya03@users.noreply.github.com>
2025-08-21 21:06:48 +0530

    Update ApprovalRepository.java

--- springapp/src/main/java/com/examly/springapp/repository/ApprovalRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/ApprovalRepository.java
@@ -2,16 +2,9 @@ package com.examly.springapp.repository;
 
 import com.examly.springapp.model.Approval;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
-
 public interface ApprovalRepository extends JpaRepository<Approval, Long> {
-
     List<Approval> findByExpenseId(Long expenseId);
-
-    // List<Approval> findByApproverId(Long approverId);
-
     List<Approval> findByApprovalStatus(String approvalStatus);
-
     List<Approval> findByIsFinalApprovalTrue();
 }
