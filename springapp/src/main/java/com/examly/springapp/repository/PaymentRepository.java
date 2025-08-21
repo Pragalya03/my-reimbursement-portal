5a3f78dddd7029307762e4886891ed36c65be480
Pragalya03 <149648960+Pragalya03@users.noreply.github.com>
2025-08-21 21:08:37 +0530

    Update PaymentRepository.java

--- springapp/src/main/java/com/examly/springapp/repository/PaymentRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/PaymentRepository.java
@@ -8,12 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
-
     List<Payment> findByExpenseId(Long expenseId);
-
     List<Payment> findByStatus(String status);
-
     List<Payment> findByPaymentMethod(String paymentMethod);
-
-    // List<Payment> findByProcessedBy(User user);
 }
