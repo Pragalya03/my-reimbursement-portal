6061bd42797e54aaa9249c96b689a932291bc87b
Pragalya03 <149648960+Pragalya03@users.noreply.github.com>
2025-08-21 21:10:56 +0530

    Update UserRepository.java

--- springapp/src/main/java/com/examly/springapp/repository/UserRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/UserRepository.java
@@ -4,16 +4,12 @@ import com.examly.springapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
     Optional<User> findByUsername(String username);
-
     Optional<User> findByEmail(String email);
-
+    
     @Query("SELECT u FROM User u WHERE u.employeeId = :employeeId")
     Optional<User> findByEmployeeId(String employeeId);
 }
