6f9b7d82ac635947ba14b2082fd628abf3e8f1ed
Pragalya03 <149648960+Pragalya03@users.noreply.github.com>
2025-08-21 21:08:54 +0530

    Update ReceiptRepository.java

--- springapp/src/main/java/com/examly/springapp/repository/ReceiptRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/ReceiptRepository.java
@@ -2,14 +2,8 @@ package com.examly.springapp.repository;
 
 import com.examly.springapp.model.Receipt;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
-
 public interface ReceiptRepository extends JpaRepository<Receipt, Long> {
-
     List<Receipt> findByExpenseId(Long expenseId);
-
     List<Receipt> findByFileType(String fileType);
-
-    //List<Receipt> findByOcrVendor(String ocrVendor);
 }
